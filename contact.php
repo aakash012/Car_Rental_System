09cbefda9c8b0362d4693b8e002b743f6a7e5bc1
aakash012 <aakash012@live.com>
2019-04-19 13:08:39 +0530

    second commit

--- contact.php
+++ contact.php
@@ -3,9 +3,7 @@
 <head>
 	<title>Car Rental</title>
 	<meta charset="utf-8">
-	<meta name="author" content="pixelhint.com">
-	<meta name="description" content="La casa free real state fully responsive html5/css3 home page website template"/>
-	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
+=	<meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0" />
 	
 	<link rel="stylesheet" type="text/css" href="css/reset.css">
 	<link rel="stylesheet" type="text/css" href="css/responsive.css">
